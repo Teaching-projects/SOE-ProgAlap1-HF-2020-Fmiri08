2ba67600e873c1373374c2ef4795c68bb43d9f03
Fmiri08 <miririn082@gmail.com>
2020-10-26 13:29:53 +0100

    zh

--- 010/main.py
+++ 010/main.py
@@ -1,13 +1,21 @@
-
-
-
-
+honap=[]
 szamla=[]
-
-
-
-
+for i in range(24):
+    szam=int(input())
+    honap.append(szam)
+index=0
+fix=int(input())
+perc=int(input())
+sms=int(input())
+while index!=24:
+    if (honap[index]*perc)+(honap[index+1]*sms)<fix:
+        szamla.append(fix)
+        index+=2
+    else : 
+        szamla.append((honap[index]*perc)+(honap[index+1]*sms)-fix)   
+        index+=2
+total=0
+for i in szamla:
+    total+=i
 print(szamla)
 print(total)
-
-
