8d31150e178466fe82de325c26a8bfef7123ddc2
Fmiri08 <miririn082@gmail.com>
2021-03-01 18:46:50 +0100

    megoldva

--- 200/my_time.py
+++ 200/my_time.py
@@ -94,7 +94,7 @@ class Time:
         """
         if self.seconds<60:
             return "{}".format(self._ss())
-        elif self.seconds>60 and self.seconds>3600:
+        elif self.seconds>60 and self.seconds<3600:
             return "{}:{:02d}".format(self._mm(), self._ss())
         else: return "{}:{:02d}:{:02d}".format(self._hh(), self._mm(), self._ss())        
 
